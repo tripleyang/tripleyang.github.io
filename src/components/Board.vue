6b32f3135febe11bcc3b4f108c0f624e6905f088
Leonid Gordenin <db0zmail@gmail.com>
2024-11-15 00:25:17 -0300

    implemented solution check

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -32,11 +32,72 @@ const isClue = (x: number, y: number) => {
   return (props.puzzle.clues[x] || {})[y] != undefined;
 };
 
+const isSolved = () => {
+  const components: Map<number, number> = new Map();
+
+  for (let y = 0; y < props.puzzle.height; y++) {
+    for (let x = 0; x < props.puzzle.width; x++) {
+      const c = getCell(x, y);
+      if (c == undefined || c < 1) {
+        console.log("not filled");
+        return false;
+      }
+
+      const n = x + y * props.puzzle.height;
+      components.set(n, n);
+
+      const leftN = n - 1;
+      const left = getCell(x - 1, y);
+      const topN = n - props.puzzle.height;
+      const top = getCell(x, y - 1);
+
+      if (c == left) {
+        components.set(n, components.get(leftN)!);
+      }
+
+      if (c == top) {
+        if (components.get(n) == n) {
+          components.set(n, components.get(topN)!);
+        } else {
+          const a = components.get(n)!;
+          const b = components.get(topN)!;
+          for (let m = 0; m <= n; m++) {
+            if (components.get(m) == a || components.get(m) == b) {
+              components.set(m, Math.min(a, b));
+            }
+          }
+        }
+      }
+
+      const topLeft = getCell(x - 1, y - 1);
+      if (c == left && c == top && c == topLeft) {
+        console.log("2x2 square");
+        return false;
+      }
+    }
+  }
+
+  const componentsSet = new Set(components.values());
+  console.log(Array.from(componentsSet.values()));
+  return componentsSet.size == 3;
+};
+
+const isSolvedRef = ref(isSolved());
+
+const checkSolved = () => {
+  isSolvedRef.value = isSolved();
+  if (isSolvedRef.value) {
+    emits("solved");
+  }
+};
+
 const setCell = (x: number, y: number, state: CellState) => {
   cells.value[x] = cells.value[x] || {};
   cells.value[x][y] = state;
 
   saveToStorage();
+
+  checkSolved();
 };
 
 const reset = () => {
@@ -46,7 +107,12 @@ const reset = () => {
 };
 </script>
 <template>
-  <div class="flex flex-col rounded-md bg-slate-200 p-0.5">
+  <div
+    class="flex flex-col rounded-md bg-slate-200 p-0.5"
+    :class="{
+      'outline outline-2 outline-offset-4 outline-lime-400': isSolvedRef,
+    }"
+  >
     <div v-for="y in props.puzzle.height" class="flex flex-row">
       <Cell
         v-for="x in props.puzzle.width"
@@ -59,14 +125,15 @@ const reset = () => {
         "
       ></Cell>
     </div>
-    <div class="flex flex-row justify-center gap-1 p-1">
+    <div class="flex flex-row items-center gap-1 p-1">
+      <div v-if="isSolvedRef" class="px-2 text-xl font-semibold">Solved!</div>
+
+      <div class="flex-1" />
+
       <AppButton
-        class="border border-slate-300 bg-slate-50"
-        @click="emits('solved')"
+        class="justify-self-end bg-red-500 font-semibold text-white"
+        @click="reset"
       >
-        Mark as solved
-      </AppButton>
-      <AppButton class="bg-red-500 font-semibold text-white" @click="reset">
         Reset
       </AppButton>
     </div>
