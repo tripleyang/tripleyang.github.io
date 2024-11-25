8a9045b107088d29ace860df7c1dd32f9716a7a8
Leonid Gordenin <db0zmail@gmail.com>
2024-11-25 15:36:56 -0300

    parity theorem

--- src/views/HelpView.vue
+++ src/views/HelpView.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import AppLink from "@/components/AppLink.vue";
+</script>
+
 <template>
   <p>[WIP]</p>
   <p
@@ -164,6 +168,210 @@
     <h1>Theorem 4</h1>
     <details>
       <summary>Hint</summary>
+      We'll call this the Parity Theorem. Unfortunately, I think this one is too
+      hard to really guide you through with hints. You can try reading about the
+      related theorem for Yin-Yang
+      <AppLink
+        href="https://puzzling.stackexchange.com/questions/53193/a-problem-about-yin-yang-puzzles"
+        >here</AppLink
+      >.
+    </details>
+    <details>
+      <summary>Proof, step 1</summary>
+      <p
+        >To get started, let's color the vertices in a checkerboard pattern like
+        this:</p
+      >
+      <div class="flex flex-row items-start gap-2"
+        ><img src="/help/help4.png" /> <img src="/help/help5.png"
+      /></div>
+      <p
+        >Notice that if the board is of a Even x Even size, there will be 1 more
+        black vertex than there will be white vertices; if the board is of a
+        different size, there will be equally many black and white vertices
+        (this includes Odd x Odd and Odd x Even boards).</p
+      >
+      <p
+        >We will need to account for this relationship. We don't really need to
+        know how many there are of each color, but it's important that there's
+        either the same number or one more black.</p
+      >
+      <p
+        >To make it easier to track this, we can introduce the following
+        concept.</p
+      >
+      <p
+        ><b>Definition.</b> For a set of vertices, its <em>value</em> is the
+        integer number equal to B - W, where B is the number of black vertices
+        in the set, and W is the number of white vertices in the set.</p
+      >
+      <p
+        >In other words, each black vertex has a value of +1, and each white
+        vertex has a value of -1.</p
+      >
+      <p
+        ><b>Lemma 1.</b> The set of all vertices has a total value of +1 on Even
+        x Even boards, and 0 otherwise. Proof is left as an exercise for the
+        reader.</p
+      >
+      <p
+        ><b>Exercise.*</b> Use the concept of <em>value</em> to prove the Parity
+        Theorem for standard Yin-Yang. Expand it by exploring what happens on
+        Odd x Odd boards.</p
+      >
+    </details>
+    <details>
+      <summary>Proof, step 2</summary>
+      <p
+        ><b>Lemma 2.</b> For any <em>chain of vertices</em>, its value only
+        depends on the color of its ends:</p
+      >
+      <ol class="list-inside list-decimal">
+        <li>A chain of the form ⚫︎-○-⋯-⚫︎-○ has value 0.</li>
+        <li>A chain of the form ⚫︎-○-⋯-○-⚫︎ has value +1.</li>
+        <li>A chain of the form ○-⚫︎-⋯-⚫︎-○ has value -1.</li>
+      </ol>
+      <p>Proof is left as an exercise for the reader.</p>
+      <p
+        ><b>Corollary.</b> Consider a set of vertices formed by the vertices of
+        a graph, whose edges are (some of) the edges of the board. The value of
+        the set doesn't depend on the colors of the vertices of degree 2; it's
+        enough to know the colors of the <em>leaves</em> (vertices with 1 edge)
+        and the <em>junctions</em> (vertices with 3 or more edges), and which of
+        them are connected by chains.
+      </p>
+      <p><b>Example.</b></p>
+      <img src="/help/help6.png" />
+      <p
+        >This graph has 2 white leaves, 1 black leaf, and 1 white junction,
+        which have a combined value of -2. The rest of the vertices are parts of
+        chains that connect them; each chain will connect a leaf to the
+        junction. The ends of the chain will have opposite colors to the colors
+        of the leaf and junction they connect; therefore, there is 1
+        ⚫︎-○-⋯-⚫︎-○ chain of value 0 and 2 ⚫︎-○-⋯-○-⚫︎ chains of value +1
+        each. Adding everything together, we get -2 + 0 + 1 + 1 = 0. This is the
+        same result we get by applying Lemma 1 to the board of size 7x7.
+      </p>
+    </details>
+    <details>
+      <summary>Proof, step 3</summary>
+      <p
+        >We've now seen that vertices of degree 2 &mdash; those that belong to
+        chains &mdash; don't affect the total value of a graph.</p
+      >
+      <p
+        >To fully exploit this fact, we can simply replace any chain in a graph
+        with a single edge. However, now the graph can have connected vertices
+        of the same color, with edges actually implying vertices of the other
+        color between them. To correct this, let's extend the definition of
+        value to graphs:</p
+      >
+      <p
+        ><b>Definition. </b> For a graph whose vertices are colored with Black
+        and White, it's <em>value</em> is defined as</p
+      >
+      <p class="text-center">B - W - BB + WW,</p>
+      <p
+        >where B and W are the numbers of Black and White vertices respectively,
+        and BB and WW are the numbers of Black-Black and White-White edges
+        respectively.</p
+      >
+      <p
+        ><b>Lemma 3.</b> The value of a graph doesn't change if any chain of
+        edges is replaced with a single edge, and the vertices between them are
+        deleted. It also doesn't change if any loop is discarded.</p
+      >
+      <p
+        >In other words, White-White edges are assigned value +1 and Black-Black
+        edges are assigned value -1 (note the opposite signs!). The graph from
+        the example can be simplified like as follows:</p
+      >
+      <img src="/help/help7.png" />
+    </details>
+    <details>
+      <summary>Proof, step 4</summary>
+      <p
+        ><b>Proposition 1. </b>The value of a graph is equal to the sum ∑v(1 -
+        d/2), taken over vertices of the graph, where v is the value of the
+        current vertex and d is its degree. Equivalently, the value is equal
+        to</p
+      >
+      <p class="text-center">1/2 v₁ - 1/2 v₃ - v₄ - ...,</p>
+      <p>where vᵢ is the total value of vertices of degree i.</p>
+      <p
+        ><b>Proof. </b> To prove this formula is correct, remove all edges of
+        the graph first, so that the graph consists only of isolated vertices.
+        In this case, the value is ∑v, which matches the formula when d=0 for
+        all vertices. Next, rebuild the graph by adding edges and observe how
+        the value of the graph changes. Adding an edge increases the degree of 2
+        vertices by 1 each, and therefore (1 - d/2) is decreased by 1/2 for 2
+        terms in the sum. If the edge is added between two vertices of opposite
+        colors, the value is unchanged; ∑v(1 - d/2) is also unchanged, because 2
+        terms in the sum will change by -v/2 with v of opposite signs. If the
+        edge is added between two black vertices, the value is decreased by 1;
+        ∑v(1 - d/2) is also decreased by 1, with 2 terms in the sum will
+        decrease by 1/2. Similarly, when the edge is added between two white
+        vertices, both increase by 1.</p
+      >
+    </details>
+    <details>
+      <summary>Proof, step 5</summary>
+      <p>Recall the diagram from Theorem 3:</p>
+      <img src="/help/help3.png" />
+      <p
+        >We can now see that this diagram also shows the graphs that are
+        obtained by taking the set of all vertices along with all splitting
+        edges as well as all border edges, and collapsing all chains. The graphs
+        (before collapsing) contain all vertices on the board due to the rule
+        that forbids 2x2 squares of the same color.</p
+      >
+      <p
+        >Now we can apply Proposition 1 along with Lemma 1. Our graphs only
+        contain vertices of degrees 3 and 4, so we'll discard unnecessary terms
+        and multiply the equation by -2. We obtain</p
+      >
+      <p><b>Parity Theorem. </b> For puzzles of Even x Even sizes, </p>
+      <p class="text-center">v₃ + 2v₄ = -2.</p>
+      <p>For puzzles of other sizes, </p>
+      <p class="text-center">v₃ + 2v₄ = 0.</p>
+    </details>
+    <details>
+      <summary>Corollaries</summary>
+      <p
+        >Denote by <em>generalized triple points</em> the vertices that are
+        either triple points or lie on the border and touch 2 different colors;
+        these are all points that have degree 3 in our graphs.</p
+      >
+      <p
+        ><b>Corollary 1. </b> If the puzzle doesn't have a quadruple point, we
+        denote B and W the number of Black and White generalized triple points
+        respectively. Then</p
+      >
+      <div class="flex flex-col">
+        <div class="self-center">
+          <p>W = B + 2, if the puzzle is Even x Even;</p>
+          <p>W = B, otherwise.</p>
+        </div>
+      </div>
+      <p
+        ><b>Corollary 2.</b> Case A* is only possible on Even x Even boards. The
+        quadruple point is always White.</p
+      >
+      <p><b>Corollary 3.</b> Case AB* admits the following possibilities:</p>
+      <ol class="list-inside list-decimal">
+        <li
+          >Puzzle is Even x Even, quadruple point is White, triple points are 1
+          Black, 1 White.</li
+        >
+        <li
+          >Puzzle is Odd, quadruple point is White, triple points are both
+          Black.</li
+        >
+        <li
+          >Puzzle is Odd, quadruple point is Black, triple points are both
+          White.</li
+        >
+      </ol>
     </details>
   </section>
 </template>
